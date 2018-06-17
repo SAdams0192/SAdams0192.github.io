0a8d754d9f48ee00b211403b1865bcc1306a8341
Sam <sadams0192@gmail.com>
2018-06-17 12:57:52 -0500

    added google analytics and no caching

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,11 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+/*
+function reload() {
+    document.getElementById('iframeid').src += '';
+} btn.onclick = reload;
+*/
 
 (function($) {
 
